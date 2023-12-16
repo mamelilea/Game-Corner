7cda868e187197bafea946daf7bd37288638177e
achmal <achmalpradiptaaditama@gmail.com>
2023-12-16 13:44:51 +0700

    profile

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from '../Component/navbar'
-import user from '../assets/profile/profile.png'
+import profile from '../assets/profile/profile.png'
 import Footer from '../Component/footer'
 
 const profile = () => {
@@ -10,7 +10,7 @@ const profile = () => {
             <div className='w-screen h-screen bg-abu  flex justify-center'>
                 <div className='w-[70rem] h-[30.5rem] bg-abu2 rounded-xl flex items-center'>
                     <div className='flex items-center justify-center h-full w-1/2'>
-                        <img src={user} alt="" />
+                        <img src={profile} alt="" />
                     </div>
                     <div className='w-1/2 flex flex-col h-52 leading-[4rem] justify-center pt-10'>
                         <p className='text-[2rem] font-semibold text-potih'>Nama &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: John Doe</p>
